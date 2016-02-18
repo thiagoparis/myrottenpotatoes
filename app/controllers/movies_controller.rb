8eaa4727042ad6fa8c9d1ee9e33e03daf056f082
Thiago Paris Salviato <thiagoparis@gmail.com>
2016-02-18 20:47:14 +0000

    Allowed the app to perform updates on the stored movies information

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -30,5 +30,16 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
 end
 
