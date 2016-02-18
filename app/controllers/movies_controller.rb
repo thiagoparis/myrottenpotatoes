758f1ee811b01f390c76f50b9233b1adb6c3b061
Thiago Paris Salviato <thiagoparis@gmail.com>
2016-02-18 14:54:11 +0000

    Including support for a New Movie form.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,15 @@ class MoviesController < ApplicationController
     id = params[:id] # Retrive movie ID from URI route
     @movie = Movie.find(id) # Lookup movie by unique ID
     # Will render app/views/movies/show.html.haml by defaylt
+    
+    #Debugging exemples
+    #raise params.inspect
+    #logger.debug(@movie.title)
+    #byebug
+  end
+
+  def new
+    # default: render 'new' view template
   end
 
 end
