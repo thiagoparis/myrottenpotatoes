3d86e6418cf1c2f4908113d17d5bb2dbcb215bb9
Thiago Paris Salviato <thiagoparis@gmail.com>
2016-02-25 00:09:36 +0000

    Added some validations on the Review model.

--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -3,6 +3,7 @@ class Moviegoer < ActiveRecord::Base
   attr_accessible :uid, :provider, :name
   
   has_many :reviews
+  has_many :movies, :through => :reviews
 
   def self.create_with_omniauth(auth)
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ActiveRecord::Base
   belongs_to :moviegoer
   attr_protected :moviegoer_id
 
+  validates :movie_id, :presence => true
+
+  validates_associated :movie
+
 end
